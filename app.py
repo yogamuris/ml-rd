843ad13e3c26f687f7c02f9daffe8ef57d36d868
adyg <yogamuris@gmail.com>
2019-05-13 10:45:46 +0700

    update model

--- app.py
+++ app.py
@@ -25,7 +25,7 @@ def predict():
 
 def ValuePredictor(to_predict_list):
     to_predict = np.array(to_predict_list).reshape(1,19)
-    loaded_model = pickle.load(open("logreg1.pkl","rb"))
+    loaded_model = pickle.load(open("final_model.pkl","rb"))
     result = loaded_model.predict(to_predict)
     return result[0]
 
